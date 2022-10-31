19cbb0de0acceb4fe902f22be1242227346052fc
dmattsson <dmattsson86@gmail.com>
2022-10-31 15:44:53 +0100

    Update keymap.c

--- keyboards/preonic/keymaps/daniel/keymap.c
+++ keyboards/preonic/keymaps/daniel/keymap.c
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_ESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG,
-  TG(5),       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA, SE_ADIA,
-  KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  BACKLIT,     KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_ENT,  KC_SPC,  RAISE,   KC_DEL,   _______, _______
+  KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PSCR,
+  KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG,
+  LT(5, KC_CAPS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA, SE_ADIA,
+  KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+  BACKLIT,        KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_BSPC, KC_ENT,  KC_SPC,  RAISE,   KC_DEL,  KC_MNXT, KC_MPLY
 ),
 
 /* Colemak
@@ -121,8 +121,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______, SE_DLR,  SE_ASTR, SE_PLUS, SE_MINS, SE_QUES, SE_EQL,  SE_LBRC, SE_LCBR, SE_RCBR, SE_RBRC, _______,
   _______, SE_AT,   SE_DQUO, SE_QUOT, SE_GRV,  SE_HASH, SE_COLN, SE_SCLN, SE_LPRN, SE_RPRN, SE_UNDS, SE_PIPE,
-  _______, SE_PERC, SE_AMPR, SE_LABK, SE_RABK, SE_EXLM, SE_SLSH, SE_BSLS, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  _______, SE_PERC, SE_AMPR, SE_LABK, SE_RABK, SE_EXLM, SE_SLSH, SE_BSLS, SE_TILD, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Raise
@@ -143,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_MNXT, KC_VOLU,
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_VOLD,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU
 ),
 
 /* Adjust (Lower + Raise)
@@ -170,11 +170,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [5] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, KC_5,    KC_6,    KC_7, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_4,    KC_5,    KC_6, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, KC_ENT,  KC_0, _______, _______, _______
 )
-
 
 
 };
